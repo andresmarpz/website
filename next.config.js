52715468cc30462379ac754f36b5e9000f3a10c9
andresmarpz <andresmarpz@gmail.com>
2021-07-02 19:29:37 -0300

    New redirect route for files

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ module.exports = {
                 source: '/bookmarks',
                 destination: 'http://bookmarks.andres.run',
                 permanent: true
+            },
+            {
+                source: '/files',
+                destination: 'http://files.andres.run',
+                permanent: true
             }
         ];
     }
