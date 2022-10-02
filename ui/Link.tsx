c5b0624f9ad6707319f03b99fba17db0fee42127
Andrés Martínez <andresmarpz@gmail.com>
2022-10-02 12:08:53 -0300

    feat: add optional css prop forwarded to Stitches'

--- ui/Link.tsx
+++ ui/Link.tsx
@@ -1,9 +1,11 @@
 import { styled } from '@/stitches.config';
+import type { CSSProperties } from '@stitches/react';
 import React, { HTMLAttributeAnchorTarget, PropsWithChildren } from 'react';
 
 interface Props {
   href?: string;
   target?: HTMLAttributeAnchorTarget;
+  css?: CSSProperties
 }
 
 const StyledLink = styled('a', {
@@ -18,8 +20,9 @@ type ComponentProps = React.HTMLProps<HTMLAnchorElement> &
   Props &
   PropsWithChildren;
 const Link = React.forwardRef<HTMLAnchorElement, ComponentProps>(
-  ({ href, target, children, onClick }, ref) => (
+  ({ href, target, children, onClick, css }, ref) => (
     <StyledLink
+			css={{ ...css }}
       ref={ref}
       onClick={onClick}
       href={href}
