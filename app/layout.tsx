46dff6f583e758d979538204576aa0492b2a1206
andresmarpz <andresmarpz@gmail.com>
2023-05-19 21:59:08 -0300

    clean up repo to iterate with Next.js 13

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import '@/styles/globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { fontSans } from '@/lib/fonts';
+import { cn } from '@/lib/utils';
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            'min-h-screen bg-background font-sans antialiased',
+            fontSans.variable
+          )}
+        >
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <div className="relative flex min-h-screen flex-col">
+              <div className="flex-1">{children}</div>
+            </div>
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  );
+}
--- ui/Flex.tsx
+++
@@ -1,5 +0,0 @@
-import { styled } from "@/stitches.config";
-
-export default styled('div', {
-	display: 'flex',
-})
