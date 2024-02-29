c009e58dd7735501cf359173597ed18cd0528978
andresmarpz <andresmarpz@gmail.com>
2024-02-28 22:35:21 -0300

    delete drizzle files

--- drizzle/migrations/0001_plain_sentry.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `posts` MODIFY COLUMN `slug` varchar(255) NOT NULL;--> statement-breakpoint
-ALTER TABLE `drizzle`.`posts` ADD CONSTRAINT `posts_slug_unique` UNIQUE(`slug`);--> statement-breakpoint
-RENAME TABLE `drizzle`.`posts` TO `posts`;
---> statement-breakpoint
-DROP DATABASE `drizzle`;
