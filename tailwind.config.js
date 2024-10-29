a421d04d5767321feade48b85be32d5a1de3a69b
andresmarpz <andresmarpz@gmail.com>
2024-10-29 19:10:00 -0300

    update some deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,11 +48,6 @@ module.exports = {
           foreground: 'hsl(var(--card-foreground))'
         }
       },
-      borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
-        sm: 'calc(var(--radius) - 4px)'
-      },
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans]
       },
