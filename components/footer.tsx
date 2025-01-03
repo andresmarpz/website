7281e192359bdda58a2e56251283db7e5a394ea1
andresmarpz <andresmarpz@gmail.com>
2025-01-03 13:51:38 -0300

    update footer again

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,41 +1,15 @@
-'use client';
-
 import { cn } from '@/lib/utils';
 import { GeistMono } from 'geist/font/mono';
+import css from './footer.module.css';
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center border-t-neutral-900 text-neutral-500 relative overflow-clip min-h-[var(--footer-text-size)]">
-      <span
-        className={cn(
-          GeistMono.className,
-          '[text-rendering:geometricPrecision] font-[900] [font-size:var(--footer-text-size)] tracking-normal absolute bottom-[calc(-1*var(--footer-text-size)/1.5)]',
-          'opacity-30'
-        )}>
+    <footer className={cn(css.footer)}>
+      <span className={cn(GeistMono.className, css['footer-text'])}>
         andresmarpz
       </span>
-
-      <div
-        style={{
-          backgroundColor: 'transparent',
-          backgroundImage: 'radial-gradient(transparent 1px, rgb(0,0,0) 1px)',
-          backgroundSize: '4px 4px',
-          backdropFilter: 'blur(3px)',
-          mask: 'linear-gradient(rgba(0, 0, 0, 0) 0%, rgb(0, 0, 0, 0.6) 100%)',
-          opacity: 1,
-          position: 'absolute',
-          inset: 0,
-          overflow: 'visible',
-          zIndex: 1,
-          flex: 'none'
-        }}></div>
-      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/30" />
-
-      <style jsx>{`
-        footer {
-          --footer-text-size: clamp(3rem, 14rem, 14vw);
-        }
-      `}</style>
+      <div className={cn(css['footer-grid'])} />
+      <div className={cn(css['footer-gradient'])} />
     </footer>
   );
 }
